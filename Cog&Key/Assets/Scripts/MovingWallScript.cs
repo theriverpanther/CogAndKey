5708cb34832ca0e1a0c3476255b39c739f3c4eae
Starchy29 <ethanball29@gmail.com>
2023-09-21 16:36:23 -0400

    implemented moving platforms, still need to fix physics

--- Cog&Key/Assets/Scripts/MovingWallScript.cs
+++ Cog&Key/Assets/Scripts/MovingWallScript.cs
@@ -2,39 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// path is defined by children with the waypoint tag
 public class MovingWallScript : MonoBehaviour
 {
-    [SerializeField] private bool LoopToStart; // false, back and forth
-    private const float MOVE_SPEED = 5.0f;
+    [SerializeField] private GameObject[] Path;
+    [SerializeField] private bool LoopPath; // false, back and forth
+    private const float MOVE_SPEED = 3.5f;
 
-    private List<Vector2> pathPoints = new List<Vector2>();
+    private List<Vector2> pathPoints;
     private int nextPointIndex;
-    private bool forward; // false: moving backwards through the path
+    private bool forward = true; // false: moving backwards through the path
 
     public KeyState InsertedKey { get; set; }
 
-    void Start()
+    void Awake()
     {
-        
+        // construct path
+        pathPoints = new List<Vector2>();
+        pathPoints.Add(transform.position);
+
+        foreach(GameObject point in Path) {
+            pathPoints.Add(point.transform.position);
+        }
     }
 
     void Update()
     {
-        Vector2 target = pathPoints[nextPointIndex];
-        float currentSpeed = MOVE_SPEED;
         if(InsertedKey == KeyState.Lock) {
             return;
         }
-        if(InsertedKey == KeyState.Fast) { 
-            currentSpeed *= 2;
+
+        Vector2 target = pathPoints[nextPointIndex];
+        float displacement = MOVE_SPEED * Time.deltaTime;
+        if(InsertedKey == KeyState.Fast) {
+            displacement *= 2;
         }
 
-        float displacement = currentSpeed * Time.deltaTime;
         if(Vector2.Distance(target, transform.position) <= displacement) {
+            // reached target point
             transform.position = target;
-
-            // go to next spot
+            NextWaypoint();
         } else {
+            // moving towards starting point
             transform.position += displacement * ((Vector3)target - transform.position).normalized;
+        }
+    }
+
+    private void NextWaypoint() {
+        if(forward) {
+            nextPointIndex++;
+
+            if(nextPointIndex > pathPoints.Count - 1) {
+                if(LoopPath) {
+                    nextPointIndex = 0;
+                } else {
+                    nextPointIndex -= 2;
+                    forward = false;
+                }
+            }
+        } else {
+            nextPointIndex--;
+
+            if(nextPointIndex < 0) {
+                if(LoopPath) {
+                    nextPointIndex = pathPoints.Count - 1;
+                } else {
+                    nextPointIndex = 1;
+                    forward = true;
+                }
+            }
+        }
     }
 }
