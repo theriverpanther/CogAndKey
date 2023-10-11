aef743772ebc7df197c43a23883fcbf0a9cde3aa
Alexa <42698056+AlexaRoseA@users.noreply.github.com>
2023-10-11 11:30:58 -0400

    Key UI
    aa

--- Cog&Key/Assets/Scripts/UI-UX/KeyShowcaser.cs
+++ Cog&Key/Assets/Scripts/UI-UX/KeyShowcaser.cs
@@ -19,17 +19,28 @@ public class KeyShowcaser : MonoBehaviour
     [SerializeField]
     List<GameObject> keyManage;
 
+    private Dictionary<KeyState, GameObject> keys = new Dictionary<KeyState, GameObject>();
+
     [SerializeField]
     GameObject mainKey;
+
+    private void Start()
+    {
+        keys.Add(KeyState.Normal, keyManage[0]);
+        keys.Add(KeyState.Fast, keyManage[0]);
+        keys.Add(KeyState.Lock, keyManage[1]);
+        keys.Add(KeyState.Reverse, keyManage[2]);
+    }
 
     /// <summary>
     /// Updates the main key to display the currently used key, in inv or disabled
     /// </summary>
     /// <param name="status">If in use or not</param>
     /// <param name="keyNumber">0, 1, 2 key values</param>
-    void MainKeyStatusUpdate(bool status, int keyNumber)
+    public void MainKeyStatusUpdate(bool status, KeyState keyState)
     {
-        if(status)
+        int keyNumber = GetPlacementNum(keyState);
+        if (status)
         {
             mainKey.GetComponent<Image>().color = colorsActive[keyNumber];
             return;
@@ -41,8 +52,32 @@ public class KeyShowcaser : MonoBehaviour
     /// Updates the smaller UI keys to display the newly picked up key as an option.
     /// </summary>
     /// <param name="keyNumber"></param>
-    void PickedUpKey(int keyNumber)
+    public void SmallKeyStatusUpdate(bool status, KeyState keyState)
     {
+        int keyNumber = GetPlacementNum(keyState);
+
+        if (status)
+        {
+            keyManage[keyNumber].GetComponent<Image>().color = colorsActive[keyNumber];
+            return;
+        }
         keyManage[keyNumber].GetComponent<Image>().color = colorsDisabled[keyNumber];
+    }
+
+    /// <summary>
+    /// returns the placement num in the big list from dictionary 
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    private int GetPlacementNum(KeyState keyState)
+    {
+        GameObject key = null;
+        bool success = keys.TryGetValue(keyState, out key);
+        Debug.Log("Found?: " + keyState.ToString());
+        if (success)
+        {
+            return keyManage.IndexOf(key);
+        }
+        return -1;
     }
 }
