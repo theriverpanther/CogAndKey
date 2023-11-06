e1306e4bb073a1f3c822d8aa2dfd5c361af03b62
Ethan Ball (RIT Student) <ejb3432@rit.edu>
2023-11-06 10:04:57 -0500

    added conveyor belt visuals

--- Cog&Key/Assets/Scripts/Levels/ConveyorBeltScript.cs
+++ Cog&Key/Assets/Scripts/Levels/ConveyorBeltScript.cs
@@ -5,9 +5,48 @@ using UnityEngine;
 public class ConveyorBeltScript : Rideable, IKeyWindable
 {
     [SerializeField] private bool clockwise;
+    [SerializeField] private GameObject TickMarkPrefab;
     private const float SHIFT_SPEED = 5.0f;
     private KeyState insertedKey = KeyState.Normal;
     private float ShiftSpeed {  get { return SHIFT_SPEED * (insertedKey == KeyState.Fast ? 2f : 1f); } }
+
+    // temp visual spin
+    private List<GameObject> topTicks = new List<GameObject>();
+    private List<GameObject> bottomTicks = new List<GameObject>();
+    private List<GameObject> leftTicks = new List<GameObject>();
+    private List<GameObject> rightTicks = new List<GameObject>();
+    private float visualTimer;
+
+    void Start()
+    {
+        Rect area = Global.GetCollisionArea(gameObject);
+
+        float tickHalfWidth = TickMarkPrefab.transform.localScale.y / 2;
+
+        // horizontal marks
+        for(float x = area.xMin + 0.5f; x < area.xMax; x += 0.5f) {
+            GameObject addedTop = Instantiate(TickMarkPrefab);
+            topTicks.Add(addedTop);
+            addedTop.transform.position = new Vector3(x, area.yMax - tickHalfWidth, 0);
+
+            GameObject addedBottom = Instantiate(TickMarkPrefab);
+            bottomTicks.Add(addedBottom);
+            addedBottom.transform.position = new Vector3(x, area.yMin + tickHalfWidth, 0);
+        }
+
+        // vertical marks
+        for(float y = area.yMin + 0.5f; y < area.yMax; y += 0.5f) {
+            GameObject addedLeft = Instantiate(TickMarkPrefab);
+            leftTicks.Add(addedLeft);
+            addedLeft.transform.position = new Vector3(area.xMin + tickHalfWidth, y, 0);
+            addedLeft.transform.rotation = Quaternion.Euler(0, 0, 90);
+
+            GameObject addedRight = Instantiate(TickMarkPrefab);
+            rightTicks.Add(addedRight);
+            addedRight.transform.position = new Vector3(area.xMax - tickHalfWidth, y, 0);
+            addedRight.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+    }
 
     void FixedUpdate() {
         CheckSideRiders();
@@ -21,6 +60,38 @@ public class ConveyorBeltScript : Rideable, IKeyWindable
                 Rigidbody2D physBod = rider.GetComponent<Rigidbody2D>();
                 physBod.AddForce(-Physics2D.gravity * physBod.gravityScale);
             }
+        }
+
+        // update temp visuals
+        float lastTime = visualTimer;
+        visualTimer += ShiftSpeed * 0.7f * Time.deltaTime * (clockwise ? 1 : -1);
+        if(visualTimer >= 0.5f || visualTimer <= -0.5f) {
+            visualTimer = 0;
+        }
+        float shift = visualTimer - lastTime;
+
+        foreach(GameObject top in topTicks) {
+            Vector3 pos = top.transform.position;
+            pos.x += shift;
+            top.transform.position = pos;
+        }
+
+        foreach(GameObject bottom in bottomTicks) {
+            Vector3 pos = bottom.transform.position;
+            pos.x -= shift;
+            bottom.transform.position = pos;
+        }
+
+        foreach(GameObject left in leftTicks) {
+            Vector3 pos = left.transform.position;
+            pos.y += shift;
+            left.transform.position = pos;
+        }
+
+        foreach(GameObject right in rightTicks) {
+            Vector3 pos = right.transform.position;
+            pos.y -= shift;
+            right.transform.position = pos;
         }
     }
 
