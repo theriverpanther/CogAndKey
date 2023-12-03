eeed962988f3015106acc25650a2d7e2e44a1a0c
Alexa Amoriello <axa9517@rit.edu>
2023-12-03 14:01:25 -0500

    added titlescreen audio and changed sliders
    works now to adjust volume with log

--- Cog&Key/Assets/Scripts/UI-UX/SetVolume.cs
+++ Cog&Key/Assets/Scripts/UI-UX/SetVolume.cs
@@ -9,6 +9,11 @@ public class SetVolume : MonoBehaviour
     // The audio mixer the slider should change
     public AudioMixer mixer;
 
+    public void Start()
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(PlayerPrefs.GetFloat("MusicVolume" + mixer.name, 1) * 20));
+    }
+
     /// <summary>
     /// Uses volume to the factor of 10 (since in decibels for mixer) 
     /// and puts it into the proper scale
@@ -16,6 +21,9 @@ public class SetVolume : MonoBehaviour
     /// <param name="value">The value from the slider</param>
     public void SetLevel(float value)
     {
+        //(-80 + value * 80)
         mixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("MusicVolume" + mixer.name, value);
+        PlayerPrefs.Save();
     }
 }
