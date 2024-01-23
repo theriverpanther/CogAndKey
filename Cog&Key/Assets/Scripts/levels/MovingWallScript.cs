b2147dc60f51709069c602eb36890cd42cc8dcfd
Ethan Ball <ejb3432@rit.edu>
2024-01-23 13:23:04 -0500

    fixed moving wall vertical jump bug. now solving buffer problem

--- Cog&Key/Assets/Scripts/levels/MovingWallScript.cs
+++ Cog&Key/Assets/Scripts/levels/MovingWallScript.cs
@@ -17,7 +17,6 @@ public class MovingWallScript : Rideable
     private Vector2 bufferedMomentum;
     private float momentumBufferTime;
     private float CurrentSpeed { get { return MOVE_SPEED * (InsertedKeyType == KeyState.Fast ? 2 : 1); } }
-    private Vector2 Momentum { get { return CurrentSpeed * (pathPoints[nextPointIndex] - (Vector2)transform.position).normalized; } }
 
     void Awake()
     {
@@ -52,26 +51,19 @@ public class MovingWallScript : Rideable
         float currentSpeed = CurrentSpeed;
         float shift = currentSpeed * Time.deltaTime;
 
+        Vector2? bumpMomentum = null;
         if(Vector2.Distance(target, transform.position) <= shift) {
             // reached target point
             transform.position = target;
-            bufferedMomentum = CurrentSpeed * (pathPoints[nextPointIndex] - (Vector2)startPosition).normalized;
+            bufferedMomentum = BecomeMomentum(pathPoints[nextPointIndex] - (Vector2)startPosition);
             momentumBufferTime = 0.2f;
             NextWaypoint();
 
             // apply vertical bump when changing from upward to down and going fast
             Vector2 newDirection = pathPoints[nextPointIndex] - (Vector2)transform.position;
-            Vector2 momentum = 1.5f * currentSpeed * (transform.position - startPosition).normalized;
-            if(InsertedKeyType == KeyState.Fast && riders.Count > 0 && newDirection.y < -0.9f && momentum.y > 0.9f) {
+            if(InsertedKeyType == KeyState.Fast && riders.Count > 0 && newDirection.y < -0.9f && bufferedMomentum.y > 0.9f) {
                 momentumBufferTime = 0f; // no buffered momentum in this case
-                for(int i = 0; i < riders.Count; i++) {
-                    riders[i].GetComponent<Rigidbody2D>().velocity += momentum;
-
-                    //if(riders[i].tag == "Player") {
-                        // switch player to fall gravity because the grounded gravity is a lot stronger and it cancells the momentum
-                        //riders[i].GetComponent<Rigidbody2D>().gravityScale = PlayerScript.FALL_GRAVITY;
-                    //}
-                }
+                bumpMomentum = bufferedMomentum;
             }
         } else {
             // moving towards target point
@@ -79,6 +71,22 @@ public class MovingWallScript : Rideable
         }
 
         CheckSideRiders();
+
+        if(bumpMomentum.HasValue) {
+            // this is placed after checking side riders to prevent granting the bump and the exit momentum together
+            for(int i = 0; i < riders.Count; i++) {
+                riders[i].GetComponent<Rigidbody2D>().velocity += bumpMomentum.Value;
+                if(riders[i].tag == "Player") {
+                    // switch player to fall gravity because the grounded gravity is a lot stronger and it cancells the momentum
+                    riders[i].GetComponent<Rigidbody2D>().gravityScale = PlayerScript.FALL_GRAVITY;
+                }
+
+                riders[i].transform.SetParent(null);
+                riders.RemoveAt(i);
+                i--;
+                Debug.Log("applied bump");
+            }
+        }
 
         if(momentumBufferTime > 0) {
             momentumBufferTime -= Time.deltaTime;
@@ -120,11 +128,13 @@ public class MovingWallScript : Rideable
 
         // keep rider momentum if moving fast
         if(InsertedKeyType == KeyState.Fast) {
-            Vector2 momentum = Momentum;
-            if(momentumBufferTime > 0) {
-                momentum = bufferedMomentum;
-            }
-            rider.GetComponent<Rigidbody2D>().velocity += (momentum.x == 0 ? 0.5f : 1f) * momentum; // less boost when upward
+            //Vector2 momentum = CurrentSpeed * (pathPoints[nextPointIndex] - (Vector2)transform.position).normalized; ;
+            //if(momentumBufferTime > 0) {
+            //    momentum = bufferedMomentum;
+            //}
+            //rider.GetComponent<Rigidbody2D>().velocity += (momentum.x == 0 ? 0.5f : 1f) * momentum; // less boost when upward
+            rider.GetComponent<Rigidbody2D>().velocity += momentumBufferTime > 0 ? bufferedMomentum : BecomeMomentum(pathPoints[nextPointIndex] - (Vector2)transform.position);
+            Debug.Log("applied exit boost");
         }
     }
 
@@ -156,5 +166,14 @@ public class MovingWallScript : Rideable
             forward = !forward;
             NextWaypoint();
         }
+    }
+
+    private Vector2 BecomeMomentum(Vector2 directionNonNorm) {
+        directionNonNorm.Normalize();
+        directionNonNorm *= CurrentSpeed;
+        if(directionNonNorm.x == 0) {
+            directionNonNorm *= 0.5f;
+        }
+        return directionNonNorm;
     }
 }
