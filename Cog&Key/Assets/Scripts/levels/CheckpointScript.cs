950f0aebb74a266e38bc8535dc9c1aa44fa027d6
Alexa Amoriello (RIT Student) <axa9517@rit.edu>
2024-01-30 11:12:55 -0500

    Revert "Checkpoint and spike implementation assets"
    This reverts commit f89c40160b02876a6cb2270c0fde49f73ed2a14d.

--- Cog&Key/Assets/Scripts/levels/CheckpointScript.cs
+++ Cog&Key/Assets/Scripts/levels/CheckpointScript.cs
@@ -6,35 +6,17 @@ public class CheckpointScript : MonoBehaviour
 {
     private Color uncheckedColor;
 
-    [SerializeField]
-    public Texture2D unlitT, litT;
-
-    [SerializeField]
-    private ParticleSystem litParticles;
-
     private void Start()
     {
         uncheckedColor = GetComponent<SpriteRenderer>().color;
-        litParticles.enableEmission = false;
     }
 
     // used to create the visual difference between the current checkpoint and an unused checkpoint
     public void SetAsCheckpoint(bool isCheckpoint) {
-        //if(isCheckpoint) {
-        //    GetComponent<SpriteRenderer>().color = Color.green;
-        //} else {
-        //    GetComponent<SpriteRenderer>().color = uncheckedColor;
-        //}
-
-        if (isCheckpoint)
-        {
-            litParticles.enableEmission = true;
-            GetComponent<SpriteRenderer>().sprite = Sprite.Create(litT, new Rect(0.0f, 0.0f, litT.width, litT.height), new Vector2(0.5f, 0.5f), 100.0f);
-        }
-        else
-        {
-            litParticles.enableEmission = false;
-            GetComponent<SpriteRenderer>().sprite = Sprite.Create(unlitT, new Rect(0.0f, 0.0f, unlitT.width, unlitT.height), new Vector2(0.5f, 0.5f), 100.0f);
+        if(isCheckpoint) {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        } else {
+            GetComponent<SpriteRenderer>().color = uncheckedColor;
         }
     }
 
