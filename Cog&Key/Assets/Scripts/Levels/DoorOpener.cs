d5e6e226baddf5a79d191a87ea782050bf5d887a
Alexa Amoriello <axa9517@rit.edu>
2024-01-31 15:01:41 -0500

    all boxes good
    aa

--- Cog&Key/Assets/Scripts/Levels/DoorOpener.cs
+++ Cog&Key/Assets/Scripts/Levels/DoorOpener.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorOpener
+public class DoorOpener : MonoBehaviour
 {
     private bool activated;
     public bool Activated {
