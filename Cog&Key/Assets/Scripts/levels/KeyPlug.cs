d92de6d3b67e9a7bb6d81467b309543d842384de
Alexa Amoriello (RIT Student) <axa9517@rit.edu>
2024-04-11 13:29:48 -0400

    added a light to the door opener
    aa

--- Cog&Key/Assets/Scripts/levels/KeyPlug.cs
+++ Cog&Key/Assets/Scripts/levels/KeyPlug.cs
@@ -8,13 +8,18 @@ public class KeyPlug : KeyWindable
 
     private DoorOpener doorLock;
 
+    private SpriteRenderer light;
+
     void Start() {
         doorLock = new DoorOpener(target);
+        light = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        light.color = Color.red;
     }
 
     protected override void OnKeyInserted(KeyState newKey) {
         if(newKey == KeyState.Fast || newKey == KeyState.Reverse) {
             doorLock.Activated = true;
+            light.color = Color.green;
         }
         else if(newKey == KeyState.Lock) {
             target.Locked = true;
@@ -24,6 +29,7 @@ public class KeyPlug : KeyWindable
     protected override void OnKeyRemoved(KeyState removedKey) {
         if(removedKey == KeyState.Fast || removedKey == KeyState.Reverse) {
             doorLock.Activated = false;
+            light.color = Color.red;
         }
         else if(removedKey == KeyState.Lock) {
             target.Locked = false;
