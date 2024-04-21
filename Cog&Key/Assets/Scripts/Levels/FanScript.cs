e82567867fa78dbf7e654cbcd6af50aa4f4fea37
Alexa Amoriello <axa9517@rit.edu>
2024-04-21 19:47:02 -0400

    Fan Particles update
    aa

--- Cog&Key/Assets/Scripts/Levels/FanScript.cs
+++ Cog&Key/Assets/Scripts/Levels/FanScript.cs
@@ -22,7 +22,8 @@ public class FanScript : KeyWindable
     private Vector2 direction;
     private float fanRange;
 
-    void Start() {
+    void Start()
+    {
         float angle = Mathf.Deg2Rad * (transform.rotation.eulerAngles.z + 90f);
         direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
         fanRange = GetComponents<BoxCollider2D>()[1].size.y;
@@ -30,14 +31,17 @@ public class FanScript : KeyWindable
         Wind1 = transform.GetChild(0).gameObject;
         Wind2 = transform.GetChild(1).gameObject;
         particles = transform.GetChild(4).GetComponent<ParticleSystem>();
-        basePos = transform.position;
+        basePos = transform.GetChild(4).transform.position;
     }
 
-    void Update() {
-        foreach(Rigidbody2D physBod in affectedEntities) {
+    void Update()
+    {
+        foreach (Rigidbody2D physBod in affectedEntities)
+        {
             float distanceFromFan = Vector3.Project(physBod.transform.position - transform.position, direction).magnitude;
             float rangeMultiplier = (fanRange - distanceFromFan) / fanRange;
-            if(rangeMultiplier < 0) {
+            if (rangeMultiplier < 0)
+            {
                 rangeMultiplier = 0;
             }
             float force = MAX_BASE_FORCE * Mathf.Sqrt(rangeMultiplier);
@@ -50,10 +54,6 @@ public class FanScript : KeyWindable
         if (particles.startSpeed == 0) emit.rateOverTime = 0;
         else emit.rateOverTime = 50;
 
-        if (InsertedKeyType != KeyState.Reverse) particles.transform.position = basePos;
-        else particles.transform.position = new Vector3(basePos.x, basePos.y + 10, basePos.z);
-
-
 
         //windTime += keyToMultiplier[InsertedKeyType] * 1.5f * Time.deltaTime;
         //float range = windTime % 1f;
@@ -62,17 +62,57 @@ public class FanScript : KeyWindable
         //Wind2.transform.localPosition = new Vector3(0.2f, range2 * fanRange, 0);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-        if(rb != null && !affectedEntities.Contains(rb)) {
+        if (rb != null && !affectedEntities.Contains(rb))
+        {
             affectedEntities.Add(rb);
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision) {
+    private void OnTriggerExit2D(Collider2D collision)
+    {
         Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-        if(affectedEntities.Contains(rb)) {
+        if (affectedEntities.Contains(rb))
+        {
             affectedEntities.Remove(rb);
         }
+    }
+
+    protected override void OnKeyInserted(KeyState newKey)
+    {
+     
+        if(newKey == KeyState.Reverse)
+        {
+            StartCoroutine(ReverseParticle("insert"));
+        }
+      
+    }
+
+    protected override void OnKeyRemoved(KeyState removedKey)
+    {
+
+        if (removedKey == KeyState.Reverse)
+        {
+            StartCoroutine(ReverseParticle("remove"));
+        }
+    }
+
+    IEnumerator ReverseParticle(string state)
+    {
+        particles.Clear();
+        particles.Stop();
+        yield return new WaitForSeconds(0.05f);
+        if(state == "remove")
+        {
+            particles.transform.position = basePos;
+        } else
+        {
+            particles.transform.position = new Vector3(basePos.x, basePos.y + 10, basePos.z);
+        }
+        particles.Play();
+        yield return new WaitForSeconds(0.05f);
+        yield return null;
     }
 }
